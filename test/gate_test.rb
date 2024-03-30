07c35f4498bdf0a6fcc9dd747b64db08bce780f8
INO <ink.avc@gmail.com>
2024-03-30 12:56:59 +0900

    [Add] capter_#7 テストをリファクタリング

--- test/gate_test.rb
+++ test/gate_test.rb
@@ -3,27 +3,27 @@ require_relative '../lib/gate'
 require_relative '../lib/ticket'
 
 class GateTest < Minitest::Test
-  def test_gate
-    umeda = Gate.new(:umeda)
-    juso = Gate.new(:juso)
-  
+  def setup
+    @umeda = Gate.new(:umeda)
+    @juso = Gate.new(:juso)
+    @mikuni = Gate.new(:mikuni)
+  end
+
+  def test_umeda_to_juso
     # 160円の切符を購入する
     ticket = Ticket.new(160)
     # 梅田で入場し、十三で出場する
-    umeda.enter(ticket)
+    @umeda.enter(ticket)
     # 期待する結果：出場できる。
-    assert juso.exit(ticket)
+    assert @juso.exit(ticket)
   end
   
-  def test_umeda_tomikuni_when_fare_is_not_enough
-    umeda = Gate.new(:umeda)
-    mikuni = Gate.new(:mikuni)
-  
+  def test_umeda_tomikuni_when_fare_is_not_enough  
     # 160円の切符を購入する
     ticket = Ticket.new(160)
     # 梅田で入場し、三国で出場する
-    umeda.enter(ticket)
+    @umeda.enter(ticket)
     # 期待する結果：出場できる。
-    refute mikuni.exit(ticket)
+    refute @mikuni.exit(ticket)
   end
 end
